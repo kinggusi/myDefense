2cbf5213a62a6c917f04db41b18880e58964e57d
SeungHyeok Lee <jjangash@gmail.com>
2026-01-17 02:11:32 +0900

    Client + Server + LFS

---
+++ server/src/main/java/com/denfense/server/DefenseServerApplication.java
@@ -0,0 +1,13 @@
+package com.denfense.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/denfense/server/DefenseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denfense.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefenseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
